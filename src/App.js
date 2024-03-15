6b8cf769a05b242fb8c5057c5f4665f53f7db2fb
LikaLikona <likalikona@gmail.com>
2024-03-15 23:46:04 +0400

    3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header/index"
+import Footer from "./components/layout/Footer/index";
 
 function App() {
   return (
