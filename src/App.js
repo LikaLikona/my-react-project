f7360883882c2a4a20946e5bee82250ac8a7088a
LikaLikona <likalikona@gmail.com>
2024-04-20 23:31:10 +0400

    e

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import "./App.css";
+import * as ReactDom from "react-dom/client";
+import {createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/layout/Header/index"
 import Footer from "./components/layout/Footer/index";
 
-function App() {
+const router =createBrowserRouter([
+  {
+    path:"/",
+    element: <h1>Home</h1>,
+    errorElement: <h1>404 Not Found</h1>
+  },
+  {
+    path:"/about",
+    element: <h1>About</h1>,
+  },
+  {
+    path: "/services",
+    element: <h1>Services</h1>,
+  },
+  {
+    path:"/contact",
+    element: <h1>Contact</h1>,
+  }
+ ]);
+ function App() {
   return (
-  <div className="App">
-    <Header/>
-    <Footer/>
+    <>
+      <Header/>
+      <RouterProvider router ={router} />
+      
+    </>
+  );
+ }
 
-  </div>
-  )
-  ;
-}
+
+// function App() {
+//   return (
+//   <div className="App">
+//     <Header/>
+//     <Footer/>
+
+//   </div>
+//   )
+//   ;
+// }
 
 export default App;
--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import NavItem from "./navItem";
+import {nav} from "../../../data/nav";
+
+
 const Header =()=> {
-    return <div>header</div>;
+    return (
+        <header>
+            <nav>
+                <ul className="flex">
+                    {nav.map((item, index)=>(
+                        <NavItem key ={index} item={item} /> 
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
 };
 export default Header;
